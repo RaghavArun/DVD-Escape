5e66156ec1aa6141047b19fdb4a04d41cd286da0
raghavarun <mathsciencevocab@gmail.com>
2026-09-17 14:51:36 -0400

    faster fireball

--- js/fireball.js
+++ js/fireball.js
@@ -1,8 +1,8 @@
 let fireballX = 700;
 let fireballY = 400;
 
-let fireballSpeedX = 3;
-let fireballSpeedY = 2.4;
+let fireballSpeedX = 6;
+let fireballSpeedY = 5;
 
 let fireballTrail = [];
 let fireballTrailCounter = 0;
